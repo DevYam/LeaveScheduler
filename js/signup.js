c453324c9a52c768158ad92be236a3781d5742fe
Divyam <divyamkumarsingh@gmail.com>
2020-06-04 14:37:11 +0530

    Added sample xhr for post call

--- js/signup.js
+++ js/signup.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("submitBtn").addEventListener('click', onSubmitClick);
 
     function onSubmitClick() {
-        alert("hi");
+        // alert("hi");
         var name = document.getElementById("name").value;
         var email = document.getElementById("email").value;
         var role = document.getElementById("role").value;
@@ -19,6 +19,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
         console.log(jsonObject);
         //TODO Pass this jsonObject to the ajax call
+
+        const dummyData = {
+            "name":"test",
+            "salary":"123",
+            "age":"23"
+        };
+
+        const jsonString = JSON.stringify(dummyData);
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                alert(xhr.responseText);
+            }
+        }
+        xhr.open("POST", "http://dummy.restapiexample.com/api/v1/create");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(jsonString);
+
+
+
+
+
 
     }
 
