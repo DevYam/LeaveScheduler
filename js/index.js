9ba35ae020fe68f37e8894b8aab3ccf3e6b92ddb
Divyam <divyamkumarsingh@gmail.com>
2020-06-05 12:38:16 +0530

    Added api calls

--- js/index.js
+++ js/index.js
@@ -1,16 +1,58 @@
-// alert("Hello");
-// function startTime() {
-//     var today = new Date();
-//     var h = today.getHours();
-//     var m = today.getMinutes();
-//     var s = today.getSeconds();
-//     m = checkTime(m);
-//     s = checkTime(s);
-//     document.getElementById('txt').innerHTML =
-//         h + ":" + m + ":" + s;
-//     var t = setTimeout(startTime, 500);
-// }
-// function checkTime(i) {
-//     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-//     return i;
-// }
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    document.getElementById("loginBtn").addEventListener('click', onLoginBtnClick);
+
+    function onLoginBtnClick() {
+        // alert("hi");
+        // var name = document.getElementById("name").value;
+        // var email = document.getElementById("email").value;
+        var uname = document.getElementById("uname").value;
+        var psw = document.getElementById("pswd").value;
+
+        var jsonObject = {
+            // "firstName": name,
+            "username": uname,
+            // "lastName": role,
+            "password": psw
+        }
+        // console.log(jsonObject);
+        // console.log(jsonObject);
+        // //TODO Pass this jsonObject to the ajax call
+
+        // const dummyData = {
+        //     "name": "test",
+        //     "salary": "123",
+        //     "age": "23"
+        // };
+
+        const jsonString = JSON.stringify(jsonObject);
+        // console.log(jsonString);
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                // alert(xhr.responseText);
+                // console.log(xhr.responseText);
+                var datatext = xhr.responseText;
+                var jsonResponse = JSON.parse(datatext);
+                // console.log("Your id is "+jsonResponse.id);
+                var status = jsonResponse.status;
+                if (status == 200){
+                    window.location.href = "loginSuccess.html";
+                }
+                else
+                    alert("Wrong Credentials")
+                // alert("Registration Successful! with response  "+jsonResponse.status );
+            }
+        }
+        xhr.open("POST", "https://6331819ebeb5.ngrok.io/users/login");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Access-Control-Allow-Origin",'*');
+        xhr.send(jsonString);
+
+
+    }
+
+
+});
+
--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,10 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     document.getElementById("submitBtn").addEventListener('click', onSubmitClick);
+    document.getElementById("loginBtn").addEventListener('click', onLoginBtnClick);
 
     function onSubmitClick() {
+
         // alert("hi");
         var name = document.getElementById("name").value;
         var email = document.getElementById("email").value;
@@ -10,9 +12,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
         var psw = document.getElementById("psw").value;
 
         var jsonObject = {
-            "name": name,
-            "email": email,
-            "role": role,
+            "firstName": name,
+            "username": email,
+            "lastName": role,
             "password": psw
         }
     // console.log(jsonObject);
@@ -36,10 +38,58 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 var datatext = xhr.responseText;
                 var jsonResponse = JSON.parse(datatext);
                 // console.log("Your id is "+jsonResponse.id);
-                alert("Registration Successful! Your id is "+jsonResponse.id );
+                alert("Registration Successful! with response  "+jsonResponse.status );
             }
         }
-        xhr.open("POST", "https://6331819ebeb5.ngrok.io/register");
+        xhr.open("POST", "https://6331819ebeb5.ngrok.io/users/signup");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Access-Control-Allow-Origin",'*');
+        xhr.send(jsonString);
+
+
+    }
+
+    function onLoginBtnClick() {
+        // var name = document.getElementById("name").value;
+        // var email = document.getElementById("email").value;
+        var uname = document.getElementById("uname").value;
+        var psw = document.getElementById("pswd").value;
+
+        var jsonObject = {
+            // "firstName": name,
+            "username": uname,
+            // "lastName": role,
+            "password": psw
+        }
+        // console.log(jsonObject);
+        // console.log(jsonObject);
+        // //TODO Pass this jsonObject to the ajax call
+
+        // const dummyData = {
+        //     "name": "test",
+        //     "salary": "123",
+        //     "age": "23"
+        // };
+
+        const jsonString = JSON.stringify(jsonObject);
+        // console.log(jsonString);
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                // alert(xhr.responseText);
+                // console.log(xhr.responseText);
+                var datatext = xhr.responseText;
+                var jsonResponse = JSON.parse(datatext);
+                var status = jsonResponse.status;
+                if (status == 200){
+                    window.location.href = "loginSuccess.html";
+                }
+                else
+                    alert("Wrong Credentials")
+            }
+        }
+        xhr.open("POST", "https://6331819ebeb5.ngrok.io/users/login");
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.setRequestHeader("Access-Control-Allow-Origin",'*');
         xhr.send(jsonString);
